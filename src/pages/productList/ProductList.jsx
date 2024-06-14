474a031a852502b1a5d54459f942fbe0e5f6595e
Mina <minamagdysh2017@gmail.com>
2024-06-14 19:50:11 +0300

    final commit

--- src/pages/productList/ProductList.jsx
+++ src/pages/productList/ProductList.jsx
@@ -2,16 +2,18 @@ import { useEffect, useState } from 'react'
 import './ProductList.css'
 import ProductCard from '../../components/ProductCard/ProductCard'
 import axios from 'axios'
+import Spinner from '../../components/spinner/Spinner'
 
 
 
 const ProductList = () => {
+  let baseURL = "https://fakestoreapi.com/products"
 
-
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
   const [products, setProducts] = useState([])
   const [categories, setCategories] = useState([])
   const [categorizedProducts, setCategorizedProducts] = useState([])
-  let baseURL = "https://fakestoreapi.com/products"
   const [activeCategory, setActiveCategory] = useState('');
   const [selectedSortBy, setSelectedSortBy] = useState('');
   const getSortedProducts = (categorizedProducts) => selectedSortBy === '' ?
@@ -39,19 +41,26 @@ const ProductList = () => {
 
 
   let getAllProducts = async () => {
-    let response = await axios.get(baseURL)
-    let prods = response.data
-    let categoriesOfAllProducts = prods.map(p => p.category)
-    setCategories([...new Set(categoriesOfAllProducts)])
-    setProducts(prods)
-    let catProducts = groupByCategory(prods)
-    setCategorizedProducts(catProducts)
+    setIsLoading(true)
+    try {
+      let response = await axios.get(baseURL)
+      let prods = response.data
+      let categoriesOfAllProducts = prods.map(p => p.category)
+      setCategories([...new Set(categoriesOfAllProducts)])
+      setProducts(prods)
+      let catProducts = groupByCategory(prods)
+      setCategorizedProducts(catProducts)
+      setIsLoading(false)
+      setError(null)
+    } catch (err) {
+      setError(err.message)
+      setIsLoading(false)
+    }
   }
 
   useEffect(() => {
     getAllProducts()
   }, [])
-
 
   return (
     <div className='container-fluid d-flex flex-column row-gap-4'>
@@ -62,47 +71,56 @@ const ProductList = () => {
         </div>
       </main>
 
-      <section className="row px-2 px-md-5">
-        <div className='categories col-12 d-flex justify-content-start align-items-center flex-wrap gap-3 w-100'>
-          <>
-            {categories.map((category) => {
+
+      {products.length && (
+        <>
+          <section className="row px-2 px-md-5">
+            <div className='categories col-12 d-flex justify-content-start align-items-center flex-wrap gap-3 w-100'>
+              <>
+                {categories.map((category) => {
+                  return (
+                    <a href={`#${category}`} key={category} className={`category-filter  ${activeCategory === category ? 'active-category-filter' : 'inactive-category-filter'}`}
+                      onClick={() => setActiveCategory(category)}
+                    >
+                      {category.toUpperCase()}
+                    </a>
+                  )
+                })}
+                <span>Sort by: </span>
+                <select className="sort-by-select-input" aria-label="Large select example" value={selectedSortBy} onChange={(e) => setSelectedSortBy(e.target.value)}>
+                  <option value=''></option>
+                  <option value='title'>Title</option>
+                  <option value='price'>Price</option>
+                </select>
+              </>
+            </div>
+
+          </section>
+
+          <section >
+            {Object.keys(categorizedProducts).map((category) => {
               return (
-                <a href={`#${category}`} key={category} className={`category-filter  ${activeCategory === category ? 'active-category-filter' : 'inactive-category-filter'}`}
-                  onClick={() => setActiveCategory(category)}
-                >
-                  {category.toUpperCase()}
-                </a>
+                <section key={category} id={`${category}`} className='row px-2 px-md-5 justify-content-start align-items-center'>
+                  <h3 className='products-category-title'>{category.toUpperCase()}</h3>
+                  {getSortedProducts(categorizedProducts[category]).map((product) => {
+                    return (
+                      < ProductCard key={product.id} product={product} />
+                    )
+                  })}
+                </section>
               )
             })}
-            <span>Sort by: </span>
-            <select className="sort-by-select-input" aria-label="Large select example" value={selectedSortBy} onChange={(e) => setSelectedSortBy(e.target.value)}>
-              <option value=''></option>
-              <option value='title'>Title</option>
-              <option value='price'>Price</option>
-            </select>
-          </>
+          </section>
+        </>
+      )}
+
+      {isLoading && (
+        <div className='d-flex justify-content-center align-items-center'>
+          <Spinner />
         </div>
+      )}
 
-      </section>
-
-      <section >
-        {Object.keys(categorizedProducts).map((category) => {
-          return (
-            <section key={category} id={`${category}`} className='row px-2 px-md-5 justify-content-start align-items-center'>
-              <h3 className='products-category-title'>{category.toUpperCase()}</h3>
-              {getSortedProducts(categorizedProducts[category]).map((product) => {
-                return (
-                  < ProductCard key={product.id} product={product} />
-                )
-              })}
-            </section>
-          )
-        })}
-      </section>
-
-      <div>
-
-      </div>
+      {error && (<p className='alert alert-danger'>Error Please try Again</p>)}
 
     </div>
   )
